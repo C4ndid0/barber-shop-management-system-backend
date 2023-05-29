f8fec21ef8b611a6674d40ec9dcde75b0de9842a
Lucas Candido <avnserver@gmail.com>
2023-05-29 02:28:00 -0300

    Start project...

---
+++ src/main/java/com/lucao/barbershopmanagementsystem/BarberShopManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.lucao.barbershopmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberShopManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarberShopManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucao/barbershopmanagementsystem/BarberShopManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucao.barbershopmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberShopManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
